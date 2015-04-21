bdf3c004520478d80b6765926630e96b0a9ca571
DocHebi <charlesbascombe54@gmail.com>
2015-04-20 20:18:46 -0400

    Create DamageSourceMS.java

---
+++ src/main/java/com/minestellar/api/frontier/DamageSourceMS.java
@@ -0,0 +1,15 @@
+package com.minestellar.api.frontier;
+
+import net.minecraft.util.DamageSource;
+
+public class DamageSourceMS extends DamageSource
+{
+
+	public static final DamageSourceMS suffocation = (DamageSourceMS) new DamageSourceMS("suffocation").setDamageBypassesArmor();
+	public static final DamageSourceMS heavyGravity = (DamageSourceMS) new DamageSourceMS("heavyGravity").setDamageBypassesArmor();
+	
+	public DamageSourceMS(String damageType)
+	{
+		super(damageType);
+	}
+}
